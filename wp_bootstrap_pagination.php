895f913f2806c33201ce5638af0ab5dfe81b0eb3
Miguel Sirvent <miguel.sirvent@gmail.com>
2016-12-03 17:52:23 -0600

    Revert "Fixed next page on numeric pagination"
    This reverts commit 0578c8c4424a1e070fdc3eac0600329b1260fc96.

--- wp_bootstrap_pagination.php
+++ wp_bootstrap_pagination.php
@@ -1,17 +1,27 @@
 <?php
 /**
- * Class Name: wp_bootsrap_pagination
- * GitHub URI: https://github.com/sigami/wp_bootsrap_pagination
- * Description: Cover all types of pagination within wordpress link pages, numeric archives and posts
- * Version: 1.2
+ * Class Name: wp_bootstrap_pagination
+ * GitHub URI: https://github.com/sigami/wp_bootstrap_pagination
+ * Description: Cover all types of pagination within WordPress. Link pages, Numeric archives and next/previous posts.
+ * Version: 1.1
  * Author: Miguel Sirvent
  */
-class  wp_bootsrap_pagination {
-	static function hooks() {
+class  wp_bootstrap_pagination {
+	private static $instance = null;
+
+	public static function instance() {
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	private function __construct() {
 		/** wp page links */
-		add_filter( 'wp_link_pages_args', __CLASS__ . '::wp_link_pages_args' );
-		add_filter( 'wp_link_pages_link', __CLASS__ . '::wp_link_pages_link', 10, 2 );
-		add_filter( 'wp_link_pages', __CLASS__ . '::wp_link_pages' );
+		add_filter( 'wp_link_pages_args', __CLASS__ . '::' . 'wp_link_pages_args' );
+		add_filter( 'wp_link_pages_link', __CLASS__ . '::' . 'wp_link_pages_link', 10, 2 );
+		add_filter( 'wp_link_pages', __CLASS__ . '::' . 'wp_link_pages' );
 	}
 
 	/**
@@ -20,7 +30,7 @@ class  wp_bootsrap_pagination {
 	 * @return mixed
 	 */
 	static function wp_link_pages_args( $args ) {
-		$args['before'] = '<nav class="text-center"><ul class="pagination"><li class="disabled"><a href="#">' . __( 'Pages:', 'maketador' ) . '</a></li>';
+		$args['before'] = '<nav class="text-center"><ul class="pagination"><li class="disabled"><a href="#">' . __( 'Pages:', 'text_domain' ) . '</a></li>';
 		$args['after']  = '</ul></nav>';
 
 		return $args;
@@ -45,7 +55,7 @@ class  wp_bootsrap_pagination {
 		$args = wp_parse_args( $args, array(
 			'prev_text'          => '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>%title',
 			'next_text'          => '%title<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>',
-			'screen_reader_text' => __( 'Post navigation', 'maketador' ),
+			'screen_reader_text' => __( 'Post navigation', 'text_domain' ),
 		) );
 
 		$navigation = '';
@@ -65,22 +75,22 @@ class  wp_bootsrap_pagination {
 		echo $navigation;
 	}
 
-	static function numeric( $args = array() ) {
+	static function numeric( $args = array()) {
 
 		$args = wp_parse_args( $args, array(
-			'prev_text'        => '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>' . __( 'Previous', 'maketador' ),
-			'next_text'        => __( 'Next', 'maketador' ) . '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>',
+			'prev_text'        => '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>' . __( 'Previous', 'text_domain' ),
+			'next_text'        => __( 'Next', 'text_domain' ) . '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>',
 			'first_page_text'  => '&laquo',
-			'first_page_title' => __( 'First', 'maketador' ),
+			'first_page_title' => __( 'First', 'text_domain' ),
 			'last_page_text'   => '&raquo;',
-			'last_page_title'  => __( 'Last', 'maketador' ),
+			'last_page_title'  => __( 'Last', 'text_domain' ),
 			'pages_to_show'    => 5,
 			'before'           => '',
 			'after'            => '',
 			'query'            => null,
 		) );
 		if ( $args['query'] instanceof WP_Query ) {
-			$wp_query = $args['query'];
+			$wp_query =  $args['query'];
 		} else {
 			global $wp_query;
 		}
@@ -135,7 +145,7 @@ class  wp_bootsrap_pagination {
 			}
 		}
 		echo '<li class="">';
-		next_posts_link( $args['next_text'], $end_page );
+		next_posts_link( $args['next_text'] );
 		echo '</li>';
 		if ( $end_page < $max_page ) {
 			$last_page_text = $args['last_page_text'];
@@ -148,25 +158,27 @@ class  wp_bootsrap_pagination {
 		//TODO
 	}
 
-	static function comments_simple( $args = array() ) {
+	static function comments_simple($args = array()){
 		$args = wp_parse_args( $args, array(
-			'screen_reader_text' => esc_html__( 'Comment navigation', 'maketador' ),
-			'next_text'          => esc_html__( 'Newer Comments %s', 'maketador' ),
-			'next_icon'          => '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>',
-			'previous_text'      => esc_html__( '%s Older Comments', 'maketador' ),
-			'previous_icon'      => '<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>',
+			'screen_reader_text'        => esc_html__( 'Comment navigation', 'text_domain' ),
+			'next_text'        => esc_html__( 'Newer Comments %s', 'text_domain' ),
+			'next_icon'=>'<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>',
+			'previous_text'  => esc_html__( '%s Older Comments', 'text_domain' ),
+			'previous_icon'=>'<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>',
 		) );
 		?>
 		<h2 class="screen-reader-text"><?php echo $args['screen_reader_text'] ?></h2>
 		<ul class="pager">
 			<li class="previous">
-				<?php previous_comments_link( sprintf( $args['previous_text'], $args['previous_icon'] ) ); ?>
+				<?php previous_comments_link( sprintf( $args['previous_text'],$args['previous_icon'] ) ); ?>
 			</li>
 			<li class="next">
-				<?php next_comments_link( sprintf( $args['next_text'], $args['next_icon'] ) ); ?>
+				<?php next_comments_link( sprintf( $args['next_text'],$args['next_icon'] ) ); ?>
 			</li>
 		</ul>
 		<?php
 	}
 
 }
+
+wp_boostrap_pagination::instance();
